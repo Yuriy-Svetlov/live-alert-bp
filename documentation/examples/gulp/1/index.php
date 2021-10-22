b532e2fe381ece1e4b53179ec0a1a79ef8e70599
semiromid <startpascal@mail.ru>
2021-10-22 14:53:09 +0300

    0.1.8

---
+++ documentation/examples/gulp/1/index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <title>Example of using live-alert-bp for Gulp</title>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width,initial-scale=1">
+    <link rel="stylesheet" href="/build/css/1.css">
+    <link rel="stylesheet" href="/build/css/2.css">
+  </head>
+   
+  <body>
+    <h1>Example of using live-alert-bp for Gulp</h1>
+
+    The example shows how this tool can be used without formatters.
+
+    <div class="bl1">bl1</div>
+    <div class="bl2">bl2</div>
+    <div class="bl3">bl3</div>
+  </body>
+</html>
